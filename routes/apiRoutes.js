53b83ed0a6008b96a23eb99ea405b52aeb659de4
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-10-17 02:24:20 -0700

    add new character post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,18 @@
 const characters = require("../data/characters")
 
 module.exports = function (app) {
+
     app.get("/api/characters", (req, res) => {
         return res.json(characters);
-
     });
+
+    app.post("/api/characters/new", (req, res) => {
+        let newCharacter = req.body;
+        newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+        characters.push(newCharacter);
+        res.json(newCharacter);
+    });
+
 
 }
 
