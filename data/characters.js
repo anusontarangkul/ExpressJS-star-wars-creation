559a1f7aa5954b6206793321d3f3ab32ef2732f2
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-10-17 01:43:46 -0700

    add html route

---
+++ data/characters.js
@@ -0,0 +1,3 @@
+const characters = [];
+
+module.exports = characters;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
+
+const characters = require("./data/characters")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`);
